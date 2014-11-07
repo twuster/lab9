fc14dd8950251d4f3a4f3864755dcb4ba369d15a
Erica <eriicaericaerica@gmail.com>
2014-11-06 20:41:12 -0800

    wat

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show, :index]
-  resources :quits
+  resources :users, only: [:show, :index] do
+    resources :quits
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
